2224f02d12dbdd6c1a64682c2a545c6ac2bae910
yesihan <1002599491@qq.com>
2024-04-03 02:07:41 +0800

    try push

---
+++ src/test/java/Add_to_bookshelf_steps.java
@@ -0,0 +1,23 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Add_to_bookshelf_steps {
+    @Given("this book not in my bookshelf")
+    public void thisBookNotInMyBookshelf() {
+    }
+
+    @When("I click the Add_to_bookshelf button")
+    public void iClickTheAdd_to_bookshelfButton() {
+        
+    }
+
+    @Then("this book in my bookshelf")
+    public void thisBookInMyBookshelf() {
+        
+    }
+
+    @Then("return warn this book already on my bookshelf and bookshelf no change")
+    public void returnWarnThisBookAlreadyOnMyBookshelfAndBookshelfNoChange() {
+    }
+}
