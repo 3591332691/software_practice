3c64c5a6f5f65c2053dbb86c39e706b79d54685d
mbm293313-h <3591332691@qq.com>
2024-04-12 22:38:06 +0800

    Changes ，请看README

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,7 +1,10 @@
 package com.example.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +13,17 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	/**
+	 * 在这里初始化数据库
+     */
+	@Bean
+	public CommandLineRunner initData(JdbcTemplate jdbcTemplate) {
+		return args -> {
+			//TODO:朋友，你就在这里写数据库的初始化就行
+			//如果没有User表，就创建一个User表 TODO：后面要改成微信授权信息（可能是微信号？）
+			jdbcTemplate.execute("DROP TABLE IF EXISTS User");
+			jdbcTemplate.execute("CREATE TABLE User (UserId INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)");
+
+		};
+	}
 }
