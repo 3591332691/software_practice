3dd9a826aa1c5643aacdc338fc03125da1a4e28d
mbm293313-l <3591332691@qq.com>
2024-04-26 14:58:32 +0800

    初始化数据库

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -1,29 +1,75 @@
 package com.example.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class BackendApplication {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+
 	/**
 	 * 在这里初始化数据库
      */
 	@Bean
-	public CommandLineRunner initData(JdbcTemplate jdbcTemplate) {
-		return args -> {
-			//TODO:朋友，你就在这里写数据库的初始化就行
-			//如果没有User表，就创建一个User表 TODO：后面要改成微信授权信息（可能是微信号？）
-			jdbcTemplate.execute("DROP TABLE IF EXISTS User");
-			jdbcTemplate.execute("CREATE TABLE User (UserId INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)");
+	public CommandLineRunner init_database(JdbcTemplate jdbcTemplate) throws Exception {
+		// 读取 SQL 文件内容
+        executeSqlFile("lab1test.sql");
+		return args -> {};
+    }
 
-		};
+	public void executeSqlFile(String filePath) {
+		try {
+			URL url = getClass().getClassLoader().getResource(filePath);
+			System.out.println("Resource URL: " + url);
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath);
+			if (inputStream != null) {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+				String line;
+				StringBuilder sqlStatement = new StringBuilder();
+				while ((line = reader.readLine()) != null) {
+					// 忽略注释和空行
+					if (line.trim().isEmpty() || line.startsWith("--")) {
+						continue;
+					}
+					sqlStatement.append(line);
+					if (line.trim().endsWith(";")) {
+						// 执行语句
+						executeSql(sqlStatement.toString());
+						// 重置语句
+						sqlStatement.setLength(0);
+					}
+				}
+				reader.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	private void executeSql(String sql) {
+		try {
+			jdbcTemplate.execute(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
 }
