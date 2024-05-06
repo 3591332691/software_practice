de7fd5a5f6eff27a94f0d6b4e4accd196617e7e0
mbm293313-l <3591332691@qq.com>
2024-05-06 12:23:31 +0800

    需要前后端同时运行在本地

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,20 +1,45 @@
 <template>
   <view>
-    个人信息页面
+    <h1>个人信息页面</h1>
+	<p>用户名:{{ UserName }}</p>
   </view>
+  
 </template>
 
+<script></script>
+
 <script>
-  export default {
-    data() {
-      return {
-        
-      }
-    },
-    methods: {
-      
+import { getUserInfo } from '../../common/js/plugin.js';
+
+export default {
+  data() {
+    return {
+      UserName: 'aa' // 初始化用户名为空字符串
+    };
+  },
+  mounted() {
+    // 在页面加载完成后，获取用户信息
+    this.fetchUserInfo();
+  },
+  methods: {
+    fetchUserInfo() {
+      // 使用之前封装的函数获取用户信息，假设函数名为getUserInfo
+      getUserInfo(1)
+        .then(userInfo => {
+		  console.log(userInfo);
+          // 将获取到的用户信息赋值给 userInfo
+          try {
+             this.UserName = userInfo.Name;
+          } catch (error) {
+              console.error('Error parsing JSON:', error);
+          }
+        })
+        .catch(error => {
+          console.error('Failed to fetch user info:', error);
+        });
     }
   }
+};
 </script>
 
 <style>
